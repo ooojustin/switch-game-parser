2f4ba5eaedd9a54fa4915ad9d18647e159e600d4
ju$tin <justin@garofolo.net>
2019-05-30 21:21:35 -0400

    started conceptual work
    btw i dont know python

--- main.py
+++ main.py
@@ -1,4 +1,50 @@
 import requests
+import json
+import urllib
+import sys
 
-print(requests.get('https://google.com'))
-print()
+url = "https://u3b6gr4ua3-dsn.algolia.net/1/indexes/*/queries?"
+url_params = {
+    'x-algolia-agent': 'Algolia for vanilla JavaScript (lite) 3.22.1;JS Helper 2.20.1',
+    'x-algolia-application-id': 'U3B6GR4UA3',
+    'x-algolia-api-key': '9a20c93440cf63cf1a7008d75f7438bf'
+}
+url += urllib.parse.urlencode(url_params);
+
+headers = {
+    'accept': 'application/json',
+    'content-type': 'application/x-www-form-urlencoded'
+}
+
+facets = ['generalFilters', 'platform', 'availability', 'categories', 'filterShops', 'virtualConsole', 'characters', 'priceRange', 'esrb', 'filterPlayers'];
+params = {
+    'query': '',
+    'hitsPerPage': 42,
+    'maxValuesPerFacet': 30,
+    'page': 1,
+    'facets': json.dumps(facets),
+    'tagFilters': ''
+}
+
+formData = {
+    'requests': [
+        {
+            'indexName': 'noa_aem_game_en_us',
+            'params': urllib.parse.urlencode(params)
+        }
+    ]
+}
+
+response = requests.post(url, json.dumps(formData), headers = headers)
+if response.status_code != 200 :
+    sys.exit()
+
+data = json.loads(response.text)
+games = data['results'][0]['hits']
+
+print(len(games))
+for game in games:
+    print(game['title'])
+
+# template:
+# https://www.metacritic.com/search/game/moonlighter/results?plats[268409]=1&search_type=advanced
